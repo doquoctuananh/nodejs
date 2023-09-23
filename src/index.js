84c69138d15f81988ecdd8d113b300d9dd617f3a
doquoctuananh <71720140+doquoctuananh@users.noreply.github.com>
2023-09-23 22:52:54 +0700

    Install morgan

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+// ghi ra cac log
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Hello World!123');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+    console.log(`Example app listening on port http://localhost:${port}`);
+});
